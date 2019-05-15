d7360d4a68255b53718f57e8258386d44e19a7e4
Garrett Weems <gwgraphicdesign@gmail.com>
2019-05-15 17:50:07 -0400

    init

---
+++ school/src/main/java/com/lambdaschool/school/controller/StudentController.java
@@ -0,0 +1,117 @@
+package com.lambdaschool.school.controller;
+
+import com.lambdaschool.school.model.ErrorDetail;
+import com.lambdaschool.school.model.Student;
+import com.lambdaschool.school.service.StudentService;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController
+{
+    @Autowired
+    private StudentService studentService;
+
+    // Please note there is no way to add students to course yet!
+
+    @GetMapping(value = "/students", produces = {"application/json"})
+    @ApiOperation(value = "returns All Students", response = Student.class, responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integr", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")})
+    public ResponseEntity<?> listAllStudents(@PageableDefault(page = 0, size = 1)Pageable pageable)
+    {
+        List<Student> myStudents = studentService.findAll(pageable);
+        return new ResponseEntity<>(myStudents, HttpStatus.OK);
+    }
+    @ApiOperation(value = "Retrieves student by student Id", response = Student.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Student Found", response = Student.class),
+            @ApiResponse(code = 404, message = "Student Not Found", response = ErrorDetail.class)
+    })
+
+    @GetMapping(value = "/Student/{StudentId}",
+                produces = {"application/json"})
+    public ResponseEntity<?> getStudentById(@ApiParam(value = "StudentId")
+            @PathVariable
+                    Long StudentId)
+    {
+        Student r = studentService.findStudentById(StudentId);
+        return new ResponseEntity<>(r, HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/student/namelike/{name}",
+                produces = {"application/json"})
+    public ResponseEntity<?> getStudentByNameContaining(
+            @PathVariable String name,
+            @PageableDefault(page = 0, size = 5) Pageable pageable)
+    {
+        List<Student> myStudents = studentService.findStudentByNameLike(name, pageable);
+        return new ResponseEntity<>(myStudents, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Creates A New Student", notes = "The Newly Created Student ID Will Be Sent To The Location Header", response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Student Created Successfully", response = void.class),
+            @ApiResponse(code = 500, message = "Error Creating Student", response = ErrorDetail.class)
+    } )
+    @PostMapping(value = "/Student",
+                 consumes = {"application/json"},
+                 produces = {"application/json"})
+    public ResponseEntity<?> addNewStudent(@Valid
+                                           @RequestBody
+                                                   Student newStudent) throws URISyntaxException
+    {
+        newStudent = studentService.save(newStudent);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newStudentURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{Studentid}").buildAndExpand(newStudent.getStudid()).toUri();
+        responseHeaders.setLocation(newStudentURI);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping(value = "/Student/{Studentid}")
+    public ResponseEntity<?> updateStudent(
+            @RequestBody
+                    Student updateStudent,
+            @PathVariable
+                    long Studentid)
+    {
+        studentService.update(updateStudent, Studentid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/Student/{Studentid}")
+    public ResponseEntity<?> deleteStudentById(
+            @PathVariable
+                    long Studentid)
+    {
+        studentService.delete(Studentid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
---
+++ school/src/main/java/com/lambdaschool/school/model/Student.java
@@ -0,0 +1,69 @@
+package com.lambdaschool.school.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "student")
+public class Student
+{
+    @ApiModelProperty(name = "StudentID", value = "Primary Student Key", required = true, example = "1")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long studid;
+
+    @ApiModelProperty(name = "Name", value = "Student Name", required = true)
+    private String studname;
+
+    @ManyToMany
+    @JoinTable(name = "studcourses",
+               joinColumns = {@JoinColumn(name = "studid")},
+               inverseJoinColumns = {@JoinColumn(name = "courseid")})
+    @JsonIgnoreProperties("students")
+    private List<Course> courses = new ArrayList<>();
+
+    public Student()
+    {
+    }
+
+    public Student(String studname)
+    {
+        this.studname = studname;
+    }
+
+    public long getStudid()
+    {
+        return studid;
+    }
+
+    public void setStudid(long studid)
+    {
+        this.studid = studid;
+    }
+
+    public String getStudname()
+    {
+        return studname;
+    }
+
+    public void setStudname(String studname)
+    {
+        this.studname = studname;
+    }
+
+    public List<Course> getCourses()
+    {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses)
+    {
+        this.courses = courses;
+    }
+}
